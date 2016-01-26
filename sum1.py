027a23c3f0b581b314350e4d57cbd2e6b7f083e4
venom <venombrend@gmail.com>
2016-01-26 22:46:15 +0200

    first

---
+++ sum1.py
@@ -0,0 +1,22 @@
+print("type integers, each followed by enter; or just enter to finish")
+
+count = 0
+sum = 0
+
+while True:
+    line = input("integer: ")
+    if line:
+        try:
+            number = int(line)
+        except ValueError as err:
+            print(err)
+            continue
+        sum += number
+        count += 1
+    else:
+        break
+
+if count:
+    print("count: ", count, " sum: ", sum)
+
+input()
